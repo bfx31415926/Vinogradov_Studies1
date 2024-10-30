437c88d6c082218e09129defa1b89318045dc0ca
bfx31415926 <smi-alexey@yandex.ru>
2024-10-30 20:25:47 +0300

    Добавил 70,71 уроки

---
+++ study71/src/main/java/com/example/study71/PrefActivity.java
@@ -0,0 +1,16 @@
+package com.example.study71;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+
+public class PrefActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        addPreferencesFromResource(R.xml.pref);
+    }
+}
